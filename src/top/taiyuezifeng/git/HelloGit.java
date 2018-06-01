02b7637dce6eebfd60230eccd9b023a2d75b65b3
taiyuezifeng <taiyuezifeng@163.com>
2018-06-01 16:08:32 +0800

    init a java project
    初始化一个Java工程

---
+++ src/top/taiyuezifeng/git/HelloGit.java
@@ -0,0 +1,22 @@
+/**
+ * HelloGit.java
+ * 2018-06-01
+ * Copyright by taiyuezifeng.
+ */
+package top.taiyuezifeng.git;
+
+/**
+ * @author taiyuezifeng
+ */
+public class HelloGit {
+
+    /**
+     * main方法
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Helle, Git!");
+
+    }
+
+}
